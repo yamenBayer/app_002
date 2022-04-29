c6cd7cc293a9a07bc7a0fb60be56dc64299e2859
Yamen Bayer <yamen.warsss@gmail.com>
2022-04-29 23:50:11 +0300

    001

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,19 +2,17 @@
 
 @section('content')
 <div class="container" style="margin-top:70px">
-    <div style="display: flex; flex-direction: column; justify-content: space-evenly;align-items:center" class="userInfo">
+    <div class="d-flex card bg-light text-dark">
 
-      <div class="pt-5" style="width:70%">
-      <center><img src="https://media-exp1.licdn.com/dms/image/C4D03AQHaaXwzWrTKTA/profile-displayphoto-shrink_200_200/0/1576884493059?e=1654128000&v=beta&t=u_MQ9ke_CGU-WyvobmTNH1Qfm3bOfRPxm_eSrI5nDAg" alt="" class="rounded-circle" style="width:40%"></center>
+      <div  style="display: flex;  justify-content: center;align-items:center;width:50%;">
+      <img src="https://media-exp1.licdn.com/dms/image/C4D03AQHaaXwzWrTKTA/profile-displayphoto-shrink_200_200/0/1576884493059?e=1654128000&v=beta&t=u_MQ9ke_CGU-WyvobmTNH1Qfm3bOfRPxm_eSrI5nDAg" alt="" class="rounded-circle" style="width:50%;border-radius: 50%; box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.8);">
       </div>
-      <div class="pt-5" style="width:70%">
-      <center>
+      <div style="display: flex;flex-direction:column;  justify-content: space-between;align-items:center;height:100%;width:50%">
        <div><h1 id="username">{{ $user->username }}</h1></div>
-       <div class="d-flex" style="justify-content: center;align-items:center">
-          <div style="padding-right: 60px"><strong>105</strong> posts</div>
-          <div><strong>625</strong> friends</div>  
+       <div style="display:flex;flex-direction:column;justify-content: center;align-items:center">
+          <div ><strong>{{ $user->profile->title }}</strong></div>
+          <div><strong>{{ $user->profile->desc }}</strong></div>  
        </div>
-      </center>  
       </div>
 
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,16 +10,12 @@
 
         <title>Shop.SY</title>
 
-       <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Scripts -->
 
 
         <!-- Bootstrap -->
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -31,68 +27,75 @@
     <body class="antialiased">
 
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="{{ route('Home') }}">SyriaShop</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="{{ route('Home') }}">Home</a></li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Categories<span class="caret"></span></a>
+    <nav class="navbar  navbar-expand-sm bg-dark navbar-dark navbar-fixed-top">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="{{ route('Home') }}">SyriaShop</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+        <a class="nav-link" href="{{ route('Home') }}">Home</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Categories</a>
           <ul class="dropdown-menu">
-            <li><a href="{{ route('getView', 'Cloth') }}">CLoth</a></li>
-            <li><a href="{{ route('getView','Technology') }}">Technology</a></li>
-            <li><a href="{{ route('getView','Food') }}">Food</a></li>
+            <li><a class="dropdown-item" href="{{ route('getView', 'Cloth') }}">CLoth</a></li>
+            <li><a class="dropdown-item" href="{{ route('getView','Technology') }}">Technology</a></li>
+            <li><a class="dropdown-item" href="{{ route('getView','Food') }}">Food</a></li>
           </ul>
         </li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">About</a></li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>  
+        <form class="d-flex">
+        <input class="form-control me-2" type="text" placeholder="Search">
+        <button class="btn btn-primary" type="button">Search</button>
+      </form>
       </ul>
+  
+
+
 
        <!-- Authentication Links -->
       @if (Route::has('login'))
       <ul class="nav navbar-nav navbar-right">
-
-     
- 
-
-
       @auth
-      <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{ Auth::user()->username }}<span class="caret"></span></a>
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">{{ Auth::user()->username }}</a>
           <ul class="dropdown-menu">
-          <li><a href="{{ url('/profile/1') }}">My Profile</a></li>   
-          <li><a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-             {{ __('Logout') }}
-          </a></li>
+            <li><a class="dropdown-item" href="{{ url('/profile/1') }}">My Profile</a></li>
+            <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+            </a></li>
           </ul>
         </li>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
+          @csrf
+        </form>
       @else
         <li>
-            <a href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+            <a class="nav-link" href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span> Login</a>
         </li>
       @if (Route::has('register'))
         <li>
-            <a href="{{ route('register') }}"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
+            <a class="nav-link" href="{{ route('register') }}"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
         </li>
         @endif
         @endauth
       </ul>
       @endif
+
+
     </div>
   </div>
 </nav>
-  
+
  
 <main class="py-4">
             @yield('content')
